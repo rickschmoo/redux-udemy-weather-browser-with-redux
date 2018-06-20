6521bd9d7d4429b5589661a7b944f7755d059a3e
Richard Boardman <rickschmoo@gmail.com>
2018-06-21 00:28:02 +0100

    Sparklines working

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
+
 // import { bindActionCreators } from 'redux';
 // import { fetchWeather } from '../actions/index';
 
@@ -10,21 +11,35 @@ class WeatherList extends Component {
 		
 		const name = cityData.city.name;
 
-		const temps = cityData.list.map(
-							weather => weather.main.temp);
-		console.log(temps);
+		const temps = _.map(cityData.list.map(
+							weather => weather.main.temp),
+							(temp) => temp - 273);
+
+		const pressures = cityData.list.map(
+							weather => weather.main.pressure);
+
+		const humidities = cityData.list.map(
+							weather => weather.main.humidity);
+
+		// console.log(temps);
 
 		return (
 			<tr key={name}>
 				<td>
 					{name}
 				</td>
+
 				<td>
-					<Sparklines height={120} width={100}
-						data={temps}>
-						<SparklinesLine color="green" />
-					</Sparklines>
+					<Chart data={temps} color='orange' units="C"/>
 				</td>
+
+				<td>
+					<Chart data={pressures} color='blue' units="HPa"/>
+				</td>
+
+				<td>
+					<Chart data={humidities} color='green' units="%"/>
+				</td>				
 			</tr>
 		);
 	}
@@ -35,9 +50,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (C)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 				<tbody>
